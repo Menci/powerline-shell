ac6d7605f4a34b1a60a84eaf9a6bbe2806b2b1d5
Menci <HuangHaoRui301@Gmail.com>
2016-12-23 14:13:14 +0800

    My theme

--- segments/root.py
+++ segments/root.py
@@ -8,5 +8,5 @@ def add_root_segment(powerline):
     fg = Color.CMD_PASSED_FG
     if powerline.args.prev_error != 0:
         fg = Color.CMD_FAILED_FG
-        bg = Color.CMD_FAILED_BG
+        bg = 3 # Color.CMD_FAILED_BG
     powerline.append(root_indicators[powerline.args.shell], fg, bg)
