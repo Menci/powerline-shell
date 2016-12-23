321a4fca8d6c74d5913e2adc59e6f75ca59257b3
Menci <HuangHaoRui301@Gmail.com>
2016-12-23 15:00:33 +0800

    My theme

--- segments/root.py
+++ segments/root.py
@@ -1,6 +1,6 @@
 def add_root_segment(powerline):
     root_indicators = {
-        'bash': ' \\$ ',
+        'bash': u' (っ･ω･)っ',
         'zsh': ' %# ',
         'bare': ' $ ',
     }
@@ -9,4 +9,5 @@ def add_root_segment(powerline):
     if powerline.args.prev_error != 0:
         fg = Color.CMD_FAILED_FG
         bg = 3 # Color.CMD_FAILED_BG
+        root_indicators['bash'] = u' (｡•́︿•̀｡) '
     powerline.append(root_indicators[powerline.args.shell], fg, bg)
